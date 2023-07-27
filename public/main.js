f22092e668e52d3e1076d2c40b5bb0382b2fec7a
Katryna Yaworski <Katrynayaworski@gmail.com>
2023-07-27 13:50:23 -0500

    test

--- public/main.js
+++ public/main.js
@@ -1,5 +1,61 @@
-const btn = document.querySelector('button')
+const btn = document.querySelector('.compliment-btn');
+const addForm = document.querySelector('form');
+const nameInput = document.querySelector('input');
+const container = document.querySelector('section');
 
-const clickHandler = () => alert('The cat name is: Rascal')
+function putTheThingInTheView(res) {
+    container.innerHTML = ''
+    nameInput.value = ''
+
+    res.data.forEach((color, index) => {
+        container.innerHTML += `<p name=${index}>${color}</p>`
+    })
+
+    document.querySelectorAll('p').forEach(element => {
+        const theIndexValue = element.getAttribute('name');
+
+        element.addEventListener('click', () => {
+            axios
+                .delete(`/api/colors/${theIndexValue}`)
+                .then(res => {
+                    putTheThingInTheView(res)
+                })
+        })
+    })
+}
+
+function submitHandler(evt) {
+    evt.preventDefault();
+
+    axios
+        .post('/api/colors', { name: nameInput.value })
+        .then(res => {
+            putTheThingInTheView(res)
+        })
+        .catch(err => {
+            nameInput.value = ''
+
+            const notif = document.createElement('aside')
+            notif.innerHTML = `<p>${err.response.data}</p>
+            <button class="close">close</button>`
+            document.body.appendChild(notif)
+
+            document.querySelectorAll('.close').forEach(btn => {
+                btn.addEventListener('click', (e) => {
+                    e.target.parentNode.remove()
+                })
+            })
+        })
+}
+
+axios
+    .get('/api/colors')
+    .then(res => {
+        putTheThingInTheView(res)
+    })
+
+addForm.addEventListener('submit', submitHandler)
+
+const clickHandler = () => alert('You are so beautiful')
 
 btn.addEventListener('click', clickHandler)
--- server/server.js
+++ server/server.js
@@ -16,9 +16,50 @@ var rollbar = new Rollbar({
 rollbar.log('Hello world!')
 
 
+const colors = [ 'blue', 'green', 'purple']
+
 app.get('/',(req,res) => {
-    rollbar.info('user has entered main page....')
     res.sendFile(path.join(__dirname,'../public/index.html'))
+})
+
+app.get('/api/colors', (req, res) => {
+    rollbar.info('Someone got the list of colors on page load')
+    res.status(200).send(colors)
+})
+
+app.post('/api/colors', function(req, res) {
+    let { name } = req.body;
+    
+    const index = colors.findIndex((color) => {
+        return color === name
+    })
+
+    try {
+        if (index === -1 && name !== "") {
+          colors.push(name);
+          rollbar.info('Someone added a color')
+          res.status(200).send(colors);
+        } else if (name === "") {
+            rollbar.error('Someone tried to enter a blank color')
+
+            res.status(400).send("must provide a name");
+        } else {
+            rollbar.error('Someone tried to enter a duplicate color name')
+          res.status(400).send("that color already exists");
+        }
+      } catch (err) {
+        console.log(err)
+        rollbar.error(err)
+      }
+})
+
+app.delete('/api/colors/:index', (req, res) => {
+    const targetIndex = +req.params.index
+
+    colors.splice(targetIndex, 1);
+
+    rollbar.info('Someone deleted a color')
+    res.status(200).send(colors)
 })
 
 app.listen(4000, () => console.log(`server running on 4000`))
