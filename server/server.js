0da9c4a1611fd56c8b9e10eecf6b78f373047fc4
Katryna Yaworski <Katrynayaworski@gmail.com>
2023-07-27 12:56:38 -0500

    rollbar added

--- server/server.js
+++ server/server.js
@@ -4,7 +4,20 @@ const path = require('path')
 
 app.use(express.static(`public`))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'df209bef5abe40638c39488c66c31ebe',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
 app.get('/',(req,res) => {
+    rollbar.info('user has entered main page....')
     res.sendFile(path.join(__dirname,'../public/index.html'))
 })
 
